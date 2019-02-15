f18f3746753440eeaf467dccbdeedae0aaeb614c
Alex Cliffe <alexander.cliffe@sap.com>
2019-02-15 15:48:09 +0000

    got icons working with FontAwesome and React, cut down css

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import Login from "./components/Login";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faIgloo } from "@fortawesome/free-solid-svg-icons";
-library.add(faIgloo);
+//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+//import { faIgloo } from "@fortawesome/free-solid-svg-icons";
+import { faUnlock, faUser } from "@fortawesome/fontawesome-free-solid";
+import { faSquare, faCheckSquare } from "@fortawesome/fontawesome-free-regular";
+library.add(faUnlock, faUser, faSquare, faCheckSquare);
 class App extends Component {
   render() {
     return (
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,58 +3,97 @@ import "./main.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default class Login extends Component {
+  state = {
+    passwordInput: false,
+    usernameInput: false,
+    rememberMe: false
+  };
+  onFocus = e => {
+    this.setState({ [e.target.name]: true });
+  };
+  onBlur = e => {
+    this.setState({ [e.target.name]: false });
+  };
+  onClick = e => {
+    this.setState({ rememberMe: !this.state.rememberMe });
+  };
   render() {
+    let passwordIcon = "icon";
+    let usernameIcon = "icon";
+    if (this.state.passwordInput) {
+      passwordIcon += " input-active";
+    }
+    if (this.state.usernameInput) {
+      usernameIcon += " input-active";
+    }
+    let rememberMeChecked = "rememberMe-check";
+    let rememberMeUnchecked = "rememberMe-uncheck";
+    if (this.state.rememberMe) {
+      rememberMeUnchecked += " invisible";
+    }
+    if (!this.state.rememberMe) {
+      rememberMeChecked += " invisible";
+    }
+
     return (
       <div>
-        <div className="wrap-login">
-          <form className="login-form validate-form" autoComplete="off">
-            <span className="login-form-title p-b-34 p-t-27">Log in</span>
+        <div className="container-login">
+          <div className="wrap-login">
+            <form className="login-form validate-form" autoComplete="off">
+              <span className="login-form-title">Log in</span>
 
-            <div
-              className="wrap-input validate-input"
-              data-validate="Enter username"
-            >
-              <input
-                className="input"
-                type="text"
-                name="username"
-                placeholder="Username"
-              />
-            </div>
+              <div className="wrap-input">
+                <input
+                  className="input inputIgloo"
+                  type="text"
+                  name="usernameInput"
+                  placeholder="Username"
+                  onFocus={this.onFocus}
+                  onBlur={this.onBlur}
+                />
+                <div className="">
+                  <FontAwesomeIcon className={usernameIcon} icon="user" />
+                </div>
+              </div>
 
-            <div
-              className="wrap-input validate-input"
-              data-validate="Enter password"
-            >
-              <input
-                className="input"
-                type="password"
-                name="pass"
-                placeholder="Password"
-              />
-              <div className="focus-input" />
-              <span className=" spanIcon" />
-              <FontAwesomeIcon className="focus-input fontIcon" icon="igloo" />
-            </div>
-
-            <div className="contact-form-checkbox">
-              <input
-                className="input-checkbox"
-                id="ckb1"
-                type="checkbox"
-                name="remember-me"
-              />
-              <label className="label-checkbox" htmlFor="ckb1">
+              <div className="wrap-input">
+                <input
+                  className="input inputIgloo"
+                  type="password"
+                  name="passwordInput"
+                  placeholder="Password"
+                  onFocus={this.onFocus}
+                  onBlur={this.onBlur}
+                />
+                <div className="">
+                  <FontAwesomeIcon className={passwordIcon} icon="unlock" />
+                </div>
+              </div>
+              <div
+                className="checkbox-icons"
+                id="checkbox-icons"
+                onClick={this.onClick}
+              >
+                <FontAwesomeIcon
+                  className={rememberMeUnchecked}
+                  icon="square"
+                />
+                <FontAwesomeIcon
+                  className={rememberMeChecked}
+                  icon="check-square"
+                />
+              </div>
+              <label className="label-checkbox" htmlFor="checkbox-icons">
                 Remember me
               </label>
-            </div>
 
-            <div className="container-login-form-btn">
-              <button className="login-form-btn">Login</button>
-            </div>
+              <div className="container-login-form-btn">
+                <button className="login-form-btn">Login</button>
+              </div>
 
-            <div className="text-center p-t-90">Forgot Password?</div>
-          </form>
+              <div className="text-center p-t-90">Forgot Password?</div>
+            </form>
+          </div>
         </div>
       </div>
     );
