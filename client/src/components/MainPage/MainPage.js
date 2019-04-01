3dc7c8e155137999a640cb5bf31fa11db5109f48
lordtwinky <alexCliffe17@gmail.com>
2019-04-01 23:10:10 +0100

    input Filter changes with old code

--- client/src/components/MainPage/MainPage.js
+++ client/src/components/MainPage/MainPage.js
@@ -5,9 +5,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import exerciseArray from "../../exercises";
 export default class MainPage extends Component {
   state = {
+    valueExerciseName: "",
     exerciseNameInput: false,
     exercises: exerciseArray,
-    updatedExercises: exerciseArray
+    updatedExercises: exerciseArray,
+    exerciseNameItem: ""
   };
   onFocus = e => {
     this.setState({ [e.target.name]: true });
@@ -15,8 +17,12 @@ export default class MainPage extends Component {
   onBlur = e => {
     this.setState({ [e.target.name]: false });
   };
-  onClick = e => {
-    this.setState({ rememberMe: !this.state.rememberMe });
+  onClick = value => {
+    this.setState({ valueExerciseName: value });
+  };
+
+  onChange = e => {
+    this.setState({ valueExerciseName: e.target.value });
   };
 
   filterExercises = e => {
@@ -37,17 +43,19 @@ export default class MainPage extends Component {
   };
 
   render() {
+    //  console.log(this.state.exerciseNameItem);
     let exerciseNamesList = "list-exerciseName";
     let exerciseNameInput = "input-exerciseName";
     if (this.state.exerciseNameInput) {
       exerciseNamesList += "-active";
+
       //wrapInputExerciseName += " input-active";
       exerciseNameInput += " icon-active";
     }
     if (!this.state.exerciseNameInput) {
-      exerciseNamesList = "list-exerciseName";
+      // exerciseNamesList = "list-exerciseName";
       //wrapInputExerciseName += " input-active";
-      exerciseNameInput += "input-exerciseName";
+      //exerciseNameInput += "input-exerciseName";
     }
     return (
       <div>
@@ -57,34 +65,55 @@ export default class MainPage extends Component {
             <div className="form-wrapper">
               <form className="form-addExercise">
                 <span className="form-title">Add exercise</span>
-                <div className="x">
-                  {/* <label htmlFor="exerciseNameInput" className="input-label">
-                    <FontAwesomeIcon
-                      className={exerciseNameIcon}
-                      icon="dumbbell"
-                    />
-                    Exercise Name
-                  </label> */}
-                  <div>
-                    <input
-                      className={exerciseNameInput}
-                      name="exerciseNameInput"
-                      type="text"
-                      placeholder="Type to filter"
-                      onKeyUp={this.filterExercises}
-                      autoComplete="off"
-                      onFocus={this.onFocus}
-                      onBlur={this.onBlur}
-                    />
-                    <ul className={exerciseNamesList}>
-                      {this.state.updatedExercises.map((exercise, key) => {
-                        return (
-                          <li key={key} value={exercise.name}>
-                            {exercise.name}
-                          </li>
-                        );
-                      })}
-                    </ul>
+                <div className="form-content-wrapper">
+                  <div className="exerciseName-wrapper">
+                    <div className="exerciseName-label-wrapper">
+                      <label
+                        htmlFor="exerciseName-input-wrapper"
+                        className="input-label"
+                      >
+                        <FontAwesomeIcon
+                          className="exerciseNameIcon"
+                          icon="dumbbell"
+                        />
+                        Exercise Name
+                      </label>
+                    </div>
+                    <div id="exerciseName-input-wrapper">
+                      <input
+                        className={exerciseNameInput}
+                        name="exerciseNameInput"
+                        type="text"
+                        placeholder="Type to filter"
+                        onKeyUp={this.filterExercises}
+                        autoComplete="off"
+                        onFocus={this.onFocus}
+                        onBlur={this.onBlur}
+                        value={this.state.valueExerciseName}
+                        onChange={this.onChange}
+                      />
+                      <ul className={exerciseNamesList}>
+                        {this.state.updatedExercises.map((exercise, key) => {
+                          return (
+                            <li
+                              key={key}
+                              value={exercise.name}
+                              onMouseDown={this.onClick.bind(
+                                this,
+                                exercise.name
+                              )}
+                              name="exerciseNameItem"
+                            >
+                              {exercise.name}
+                            </li>
+                          );
+                        })}
+                        {/* onMouseDown function is triggered when the mouse button is held down
+                      (onMouseUp function is triggered when the mouse button is released)
+                      onMouseDown is used instead of onClick because onClick would be executed after
+                      the onBlur of the input, meaning that it would never be executed. */}
+                      </ul>
+                    </div>
                   </div>
                 </div>
               </form>
